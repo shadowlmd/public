d49f8b26974b1d21043a621b9fd121cdf2e1862f
Aleksei Faians <af.git@bsrealm.net>
2017-10-15 20:28:43 +0300

    pew pew

--- cloudlinux/lventop/lventop.py
+++ cloudlinux/lventop/lventop.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import os
+
+min_lve = 500
+
 for lve_id in os.listdir('/proc/lve/per-lve/'):
-  if os.path.isfile(os.path.join('/proc/lve/per-lve', lve_id, 'net_stat')):
+  if lve_id.isdigit() and int(lve_id) >= min_lve and os.path.isfile(os.path.join('/proc/lve/per-lve', lve_id, 'net_stat')):
     print(lve_id)
