287703e0ac685850edbbda55249a86ae711e2fa2
Vasya Pupkin <vpupkin@bsrealm.net>
2017-11-05 20:43:21 +0300

    lventop.py: configurable refresh interval; don't show incorrect numbers on first run

--- cloudlinux/lventop/lventop.py
+++ cloudlinux/lventop/lventop.py
@@ -7,9 +7,11 @@ def main(stdscr):
     stdscr.nodelay(1)
 
     min_lve = 500
+    interval = 5
     sort_pri = 'out'
     sort_sec = 'in'
     oldvals = {}
+    filled = False
 
     def bytes_to_human(num):
         for unit in ['KiB', 'MiB']:
@@ -54,22 +56,28 @@ def main(stdscr):
                 continue
             if not lve_id in oldvals:
                 oldvals[lve_id] = { 'in': 0, 'out': 0 }
-            bandwidth[lve_id] = { 'in': traf_in - oldvals[lve_id]['in'], 'out': traf_out - oldvals[lve_id]['out'] }
+            bandwidth[lve_id] = {
+                'in': (traf_in - oldvals[lve_id]['in']) / float(interval),
+                'out': (traf_out - oldvals[lve_id]['out']) / float(interval)
+            }
             oldvals[lve_id] = { 'in': traf_in, 'out': traf_out }
         for lve_id in oldvals:
             if not lve_id in bandwidth:
                 oldvals[lve_id] = { 'in': 0, 'out': 0 }
-        lines = 0
-        for lve_id in sorted(bandwidth, key = sort_func, reverse = True):
-            if bandwidth[lve_id]['in'] == 0 and bandwidth[lve_id]['out'] == 0:
-                continue
-            lines += 1
-            stdscr.addstr('\n%9s' % (lve_id) + '%15s' % (bytes_to_human(bandwidth[lve_id]['in'])) + '%15s' % (bytes_to_human(bandwidth[lve_id]['out'])));
-            if lines == stdscr.getmaxyx()[0]-1:
-                break
+        if filled:
+            lines = 0
+            for lve_id in sorted(bandwidth, key = sort_func, reverse = True):
+                if bandwidth[lve_id]['in'] == 0 and bandwidth[lve_id]['out'] == 0:
+                    continue
+                lines += 1
+                stdscr.addstr('\n%9s' % (lve_id) + '%15s' % (bytes_to_human(bandwidth[lve_id]['in'])) + '%15s' % (bytes_to_human(bandwidth[lve_id]['out'])));
+                if lines == stdscr.getmaxyx()[0]-1:
+                    break
+        else:
+            filled = True
         stdscr.move(0, 0)
         stdscr.refresh()
-        time.sleep(1)
+        time.sleep(interval)
 
 if __name__ == '__main__':
     curses.wrapper(main)
