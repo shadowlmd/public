3bb4b883dc9e1a9660d941f61176bd1bd8943d27
Vasya Pupkin <pupkin@bsrealm.net>
2018-02-19 17:09:31 -0500

    lventop.py: almost instant response to keypresses

--- cloudlinux/lventop/lventop.py
+++ cloudlinux/lventop/lventop.py
@@ -8,10 +8,12 @@ def main(stdscr):
 
     min_lve = 500
     interval = 3
+    sleep_cycles = interval * 10
     sort_pri = 'out'
     sort_sec = 'in'
     oldvals = {}
     filled = False
+    refresh = True
 
     def bytes_to_human(num):
         for unit in ['KiB', 'MiB']:
@@ -39,11 +41,20 @@ def main(stdscr):
             elif c == ord('i'):
                 sort_pri = 'in'
                 sort_sec = 'out'
+                refresh = True
             elif c == ord('o'):
                 sort_pri = 'out'
                 sort_sec = 'in'
+                refresh = True
             elif c == ord('q'):
                 return
+        if not refresh:
+            time.sleep(0.1)
+            sleep_cycles -= 1
+            if sleep_cycles == 0 or not filled:
+                refresh = True
+                filled = True
+            continue
         bandwidth = {}
         stdscr.clear()
         stdscr.addstr('   LVE ID             IN            OUT', curses.A_REVERSE)
@@ -62,33 +73,30 @@ def main(stdscr):
             traf_out = int(traf_out)
             if traf_in == 0 and traf_out == 0:
                 continue
-            if not lve_id in oldvals:
-                oldvals[lve_id] = { 'in': 0, 'out': 0 }
-            bandwidth[lve_id] = {
-                'in': (traf_in - oldvals[lve_id]['in']) / float(interval),
-                'out': (traf_out - oldvals[lve_id]['out']) / float(interval)
-            }
+            if not filled:
+                bandwidth[lve_id] = { 'in': 0, 'out': 0 }
+            else:
+                sleep_time = (interval / 0.1) - sleep_cycles
+                bandwidth[lve_id] = {
+                    'in': (traf_in - oldvals[lve_id]['in']) * 10 / sleep_time,
+                    'out': (traf_out - oldvals[lve_id]['out']) * 10 / sleep_time
+                }
             oldvals[lve_id] = { 'in': traf_in, 'out': traf_out }
         for lve_id in oldvals:
             if not lve_id in bandwidth:
                 oldvals[lve_id] = { 'in': 0, 'out': 0 }
-        if filled:
-            interval = newinterval
-            lines = 0
-            for lve_id in sorted(bandwidth, key = sort_func, reverse = True):
-                if bandwidth[lve_id]['in'] == 0 and bandwidth[lve_id]['out'] == 0:
-                    continue
-                lines += 1
-                stdscr.addstr('\n%9s' % (lve_id) + '%15s' % (bytes_to_human(bandwidth[lve_id]['in'])) + '%15s' % (bytes_to_human(bandwidth[lve_id]['out'])));
-                if lines == stdscr.getmaxyx()[0]-1:
-                    break
-        else:
-            filled = True
-            newinterval = interval
-            interval = 0.1
+        lines = 0
+        for lve_id in sorted(bandwidth, key = sort_func, reverse = True):
+            if bandwidth[lve_id]['in'] == 0 and bandwidth[lve_id]['out'] == 0:
+                continue
+            lines += 1
+            stdscr.addstr('\n%9s' % (lve_id) + '%15s' % (bytes_to_human(bandwidth[lve_id]['in'])) + '%15s' % (bytes_to_human(bandwidth[lve_id]['out'])));
+            if lines == stdscr.getmaxyx()[0]-1:
+                break
         stdscr.move(0, 0)
         stdscr.refresh()
-        time.sleep(interval)
+        refresh = False
+        sleep_cycles = interval * 10
 
 if __name__ == '__main__':
     curses.wrapper(main)
