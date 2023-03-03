3c2fad1de6292840e2cacc81d72f4ebf5776bb0c
Aleksei Faians <af.git@bsrealm.net>
2023-03-03 18:40:51 +0300

    Add placeholders

--- find_closest_sum_combo/find_closest_sum_combo.php
+++ find_closest_sum_combo/find_closest_sum_combo.php
@@ -27,9 +27,9 @@
 
         <form action="" method="post">
             Потолок суммы чисел:
-            <input type="number" name="goal"><br>
+            <input type="number" name="goal" placeholder="Например: 300"><br>
             Числа:<br>
-            <textarea name="numbers" cols="10" rows="10"></textarea><br>
+            <textarea name="numbers" cols="10" rows="10" placeholder="<?= "Например:\n\n12.3\n46\n73.00\n21,34\n..." ?>"></textarea><br>
             <input type="submit" value="Поехали!" name="submit">
         </form>
 
