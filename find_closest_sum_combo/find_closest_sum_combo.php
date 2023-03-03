5f49d1411ef56ba309676d58278cdfe133dd856c
Aleksei Faians <af.git@bsrealm.net>
2023-03-03 17:53:37 +0300

    Pew

--- find_closest_sum_combo/find_closest_sum_combo.php
+++ find_closest_sum_combo/find_closest_sum_combo.php
@@ -8,8 +8,8 @@
 <body>
     <?php
     if (isset($_POST["submit"])) {
-        $numbers = preg_split('/\s+/', $_POST["numbers"]);
-        exec("timeout -k 1 60 " . __DIR__ . "/find_closest_sum_combo.py " . escapeshellcmd($_POST["goal"]) . " " . escapeshellcmd(implode(" ", $numbers)), $out, $rc);
+        $numbers = preg_split('/\s+/', str_replace(",", ".", $_POST["numbers"]));
+        exec("timeout -k 1 10 " . __DIR__ . "/find_closest_sum_combo.py " . escapeshellcmd($_POST["goal"]) . " " . escapeshellcmd(implode(" ", $numbers)), $out, $rc);
         if ($rc == 0 && count($out) > 0) {
             $out = implode("</p>\n<p>", $out);
             echo "<p>{$out}</p>\n";
@@ -26,7 +26,7 @@
         <form action="" method="post" enctype="multipart/form-data">
             Потолок суммы чисел:
             <input type="number" name="goal"><br>
-            Числа (по числу на строчку):<br>
+            Числа:<br>
             <textarea name="numbers" cols="10" rows="10"></textarea><br>
             <input type="submit" value="Поехали!" name="submit">
         </form>
