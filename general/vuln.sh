febf7aa4adf967102c832173892ebd1caeae65a1
Aleksei Faians <af.git@bsrealm.net>
2024-04-30 01:08:29 +0800

    pew

--- general/vuln.sh
+++ general/vuln.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+N=0
+
 for F in /sys/devices/system/cpu/vulnerabilities/*; do
-    printf "%-21s: " "${F##*/}"
-    cat "$F"
+	S=${F##*/}
+	I=${#S}
+	[[ $I -gt $N ]] && N=$I
+done
+
+((N++))
+
+for F in /sys/devices/system/cpu/vulnerabilities/*; do
+	printf "%-${N}s: " "${F##*/}"
+	cat "$F"
 done
