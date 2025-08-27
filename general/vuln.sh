571ccaaf24dac1456ce8e43b6cafb5a3af3f8134
Aleksei Faians <af.git@bsrealm.net>
2025-08-27 16:29:00 +0800

    pew

--- general/vuln.sh
+++ general/vuln.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+declare -A V
+C=0
 N=0
+SKIP_NOT_AFFECTED=""
+SKIP_FULLY_MITIGATED=""
+
+shopt -s nocasematch
 
 for F in /sys/devices/system/cpu/vulnerabilities/*; do
 	S=${F##*/}
 	I=${#S}
 	[[ $I -gt $N ]] && N=$I
+	V[$S]=$(<$F)
 done
 
 ((N++))
 
-for F in /sys/devices/system/cpu/vulnerabilities/*; do
-	printf "%-${N}s: " "${F##*/}"
-	cat "$F"
+for K in "${!V[@]}"; do
+	if [[ "${V[$K]}" == "Not affected" ]]; then
+		NOT_AFFECTED="y"
+	elif [[ ! "${V[$K]}" =~ (Vulnerable|unknown) ]]; then
+		FULLY_MITIGATED="y"
+	fi
+	if	! [[ "$SKIP_NOT_AFFECTED" == "y" && "$NOT_AFFECTED" == "y" ]] && \
+		! [[ "$SKIP_FULLY_MITIGATED" == "y" && "$FULLY_MITIGATED" == "y" ]]
+	then
+		printf "%-${N}s: %s\n" "$K" "${V[$K]}"
+		((C++))
+	fi
+	if [[ $C -eq 0 ]]; then
+		echo "No matching vulnerabilities."
+	fi
 done
