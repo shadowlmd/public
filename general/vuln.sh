a70eb437ba6f087c74de58cec8f89eaaa65ee9f7
Aleksei Faians <af.git@bsrealm.net>
2025-08-27 16:39:30 +0800

    pew

--- general/vuln.sh
+++ general/vuln.sh
@@ -3,10 +3,16 @@
 declare -A V
 C=0
 N=0
-SKIP_NOT_AFFECTED=""
-SKIP_FULLY_MITIGATED=""
+SKIP_NOT_AFFECTED="y"
+SKIP_FULLY_MITIGATED="n"
 
 shopt -s nocasematch
+
+if [[ $1 == "--all" ]]; then
+	SKIP_NOT_AFFECTED="n"
+elif [[ $1 == "--skip-mitigated" ]]; then
+	SKIP_FULLY_MITIGATED="y"
+fi
 
 for F in /sys/devices/system/cpu/vulnerabilities/*; do
 	S=${F##*/}
@@ -18,16 +24,19 @@ done
 ((N++))
 
 for K in "${!V[@]}"; do
+	unset NOT_AFFECTED
+	unset FULLY_MITIGATED
 	if [[ ${V[$K]} == "Not affected" ]]; then
 		NOT_AFFECTED="y"
 	elif [[ ! ${V[$K]} =~ (Vulnerable|unknown) ]]; then
 		FULLY_MITIGATED="y"
 	fi
-	if ! [[ $SKIP_NOT_AFFECTED == "y" && $NOT_AFFECTED == "y" ]] && ! [[ $SKIP_FULLY_MITIGATED == "y" && $FULLY_MITIGATED == "y" ]]; then
-		printf "%-${N}s: %s\n" "$K" "${V[$K]}"
-		((C++))
-	fi
-	if [[ $C -eq 0 ]]; then
-		echo "No matching vulnerabilities."
-	fi
+	[[ $SKIP_NOT_AFFECTED == "y" && $NOT_AFFECTED == "y" ]] && continue
+	[[ $SKIP_FULLY_MITIGATED == "y" && $FULLY_MITIGATED == "y" ]] && continue
+	printf "%-${N}s: %s\n" "$K" "${V[$K]}"
+	((C++))
 done
+
+if [[ $C -eq 0 ]]; then
+	echo "No matching vulnerabilities."
+fi
