21b83f4916ce4a21bfcc863b282a112c1332ebfa
Vasya Pupkin <bs@bsrealm.net>
2025-11-22 04:01:02 +0800

    Switch from autopep8/isort to ruff

--- general/pyformat.sh
+++ general/pyformat.sh
@@ -1,8 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+
+RUFF_LINE_LENGTH=160
+RUFF_SELECT=ALL
+RUFF_IGNORE=BLE001,D100,D101,D103,D107,D203,D213,EM101,RUF001,S110,T201
 
 [[ $1 =~ \.py$ ]] || exit 1
 
-echo "Formatting $1 with isort and autopep8.."
+echo "Formatting $1 with ruff.."
+ruff --config "line-length = $RUFF_LINE_LENGTH" format --no-cache "$1"
 
-isort -l 150 "$1"
-autopep8 --aggressive -i --max-line-length 150 "$1"
+echo "Checking and fixing $1 with ruff.."
+ruff --config "line-length = $RUFF_LINE_LENGTH" check --select $RUFF_SELECT --ignore $RUFF_IGNORE --no-cache --fix "$1"
