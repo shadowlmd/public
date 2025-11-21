2206be91c3f3718825cd1539bef4ef4a86ec4e97
Aleksei Faians <af.git@bsrealm.net>
2025-11-21 21:53:40 +0800

    Ruffactoring

--- checks/check-dot.py
+++ checks/check-dot.py
@@ -2,25 +2,28 @@
 
 import subprocess
 import sys
+from pathlib import Path
+
+SCRIPTNAME = Path(__file__).name
 
 
-def trailing_dot(name):
-    return name.rstrip('.') + '.'
+def trailing_dot(name: str) -> str:
+    return name.rstrip(".") + "."
 
 
-def main():
+def main() -> None:
     if len(sys.argv) != 4:
-        print("Usage: python3 script.py <DOMAIN> <SERVER> <PORT>")
-        exit(1)
+        print(f"Usage: python3 {SCRIPTNAME} <DOMAIN> <SERVER> <PORT>")
+        sys.exit(1)
 
     domain = trailing_dot(sys.argv[1])
     server = trailing_dot(sys.argv[2])
     port = sys.argv[3]
 
-    command = ["dig", "+short", "+tls", "-p", port, 'a', domain, f"@{server}"]
+    command = ["dig", "+short", "+tls", "-p", port, "a", domain, f"@{server}"]
 
-    result = subprocess.run(command)
-    exit(result.returncode)
+    result = subprocess.run(command, check=False)
+    sys.exit(result.returncode)
 
 
 if __name__ == "__main__":
