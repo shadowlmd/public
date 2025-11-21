3f5b0195700ab6f1e58ed926ff12e62b4ad0661b
Vasya Pupkin <bs@bsrealm.net>
2025-11-22 04:04:24 +0800

    pew

--- general/pyformat.sh
+++ general/pyformat.sh
@@ -7,7 +7,7 @@ RUFF_IGNORE=BLE001,D100,D101,D103,D107,D203,D213,EM101,RUF001,S110,T201
 [[ $1 =~ \.py$ ]] || exit 1
 
 echo "Formatting $1 with ruff.."
-ruff --config "line-length = $RUFF_LINE_LENGTH" format --no-cache "$1"
+ruff --isolated --config "line-length = $RUFF_LINE_LENGTH" format --no-cache "$1"
 
 echo "Checking and fixing $1 with ruff.."
-ruff --config "line-length = $RUFF_LINE_LENGTH" check --select $RUFF_SELECT --ignore $RUFF_IGNORE --no-cache --fix "$1"
+ruff --isolated --config "line-length = $RUFF_LINE_LENGTH" check --select $RUFF_SELECT --ignore $RUFF_IGNORE --no-cache --fix "$1"
